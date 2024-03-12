e8c3c95da85319f741e0f1e9c28ddbcf0f8e2702
“Alex” <hello@qq.com>
2024-03-12 23:21:33 +0800

    更改项目名称

--- src/main/java/com/alex/QLLearnApplication.java
+++ src/main/java/com/alex/QLLearnApplication.java
@@ -3,16 +3,15 @@ package com.alex;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan(basePackages = "com.alex.dao")
 @SpringBootApplication
 @EnableTransactionManagement
-public class SpringbootMybatisApplication {
+public class QLLearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootMybatisApplication.class, args);
+        SpringApplication.run(QLLearnApplication.class, args);
     }
 
 }
